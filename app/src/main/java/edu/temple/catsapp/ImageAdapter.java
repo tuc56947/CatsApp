dc36e3ebe4cd7bfc39a1bc3bc508c18143eb6a70
David Lee <tuc56947@temple.edu>
2021-02-23 20:52:40 -0500

    created custom adapter ImageAdapter

---
+++ app/src/main/java/edu/temple/catsapp/ImageAdapter.java
@@ -0,0 +1,43 @@
+package edu.temple.catsapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ImageAdapter extends BaseAdapter {
+
+    Context context;
+    ArrayList<String> items;
+
+    public ImageAdapter(Context context, ArrayList items){
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView textView = new TextView(context);
+        textView.setText(items.get(position));
+
+        return textView;
+    }
+}
